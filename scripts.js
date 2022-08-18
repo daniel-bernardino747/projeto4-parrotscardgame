109e190bcbf8360c55f3c8656b2a25d98db05d8e
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-08-18 15:16:01 -0300

    no turn if pair

--- scripts.js
+++ scripts.js
@@ -1,26 +1,44 @@
 const cards = document.querySelectorAll('.card');
 
 let firstCard, secondCard;
-let cont = 1;
+let contadorCardClick = 0;
+let pairCard = [];
 
-for (let card of cards) {
-    card.addEventListener('click', function() {
+function flipCard(blabla) {
 
-        const front = this.querySelector('.front-card');
-        const back = this.querySelector('.back-card');
-        front.classList.toggle('hidden')
-        back.classList.toggle('unhidden');
+    const front = blabla.querySelector('.front-card');
+    const back = blabla.querySelector('.back-card');
+    console.log(front, back)
+    contadorCardClick++;
 
-        if (cont === 1) {
-            firstCard = this;
-            cont++;
-        } else {
-            secondCard = this;
-        }
+    blabla.classList.add('flip');
+    front.classList.add('turned');
+    back.classList.add('turned');
+    /* back.classList.toggle('unhidden'); */
 
-        if (firstCard.dataset.card === secondCard.dataset.card) {
-            alert('deu match!')
-        }
-    });
+    pairCard.push(blabla);
+
+    if (pairCard.length === 2) {
+        console.log(pairCard);
+        checkPair();
+    }
+}
+
+function checkPair() {
+    if (pairCard[0].dataset.card === pairCard[1].dataset.card) {
+        pairCard[0].classList.add('disabled');
+        pairCard[1].classList.add('disabled');
+
+        checkPair.pop();
+        checkPair.pop();
+    } else {
+        setTimeout(() => {
+            blabla.classList.remove('flip');
+            checkPair[0].classList.remove("turned");
+            checkPair[1].classList.remove("turned");
+        
+            /* resetCards(); */
+          }, 1000);
+    }
 }
 
